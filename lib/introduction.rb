d0769d287fd8b1f4d3fc6aaed33997f8e179fb7a
zara ahmadi <zatokey@gmail.com>
2020-05-07 20:59:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language = javascritp)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 introduction(zara)

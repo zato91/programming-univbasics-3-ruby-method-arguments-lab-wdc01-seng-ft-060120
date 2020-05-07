4610d7200fef254f4ff14f677ce9b43fe6983f54
zara ahmadi <zatokey@gmail.com>
2020-05-07 21:01:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
-def introduction_with_language_optional(name, language = Ruby)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language_optional(zara)
